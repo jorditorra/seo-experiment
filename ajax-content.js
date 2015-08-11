e2a34c1caf451fa4eadae9eb89d28ab486fd1db4
marcosbermejo <marcosbermejom@gmail.com>
2015-08-11 16:29:03 +0200

    initial commit

---
+++ ajax-content.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+
+  $("#content").append("  <p>En cambio, con la gestión ágil de proyectos, los valores que se potencian son otros:\
+    adaptación al cambio, feedback rápido, entrega continua y mejora continua.\
+    Con esto, la gestión ágil aconseja planificar por periodos cortos (no más de un mes),\
+    esto significa definir, diseñar, programar, testear y entregar aquello que se pueda tener\
+    funcionando en menos de un mes. Reduciendo alcance conseguiremos mayor frecuencia de entrega.</p>\
+    <p>Tras cada entrega, necesitamos feedback del cliente. Tras cada entrega revisamos si\
+    el proyecto cumple las expectativas del cliente. De este feedback surge un debate donde\
+    se decide si se continua con el plan previsto o si rectificamos el rumbo que está tomando\
+    el proyecto y cambiamos funcionalidades. Tras este debate se decide qué funcionalidad se va a\
+    entregar para la próxima revisión.</p>\
+    <p>Como vemos, siguendo este plan empírico, iterativo e incremental conseguimos reducir\
+    el desperdicio de recursos (no invertimos tiempo en definir y diseñar funcionalidades\
+    que puedan acabar no implementándose) y aumentamos la colaboración y confianza del cliente\
+    (cada poco tiempo, el cliente puede ver avances y se le da la oportunidad de solicitar cambios\
+    de prioridad o cambios de funcionalidad).</p>\
+    <p>Adicionalmente, si dotamos al equipo de la posibilidad de trabajar juntos a cada entrega con el cliente,\
+    y si les damos la posibilidad de verse las caras con el cliente y enseñar el desarrollo que han hecho,\
+    conseguimos un equipo mucho más motivado e involucrado en el proyecto.</p>\
+    <p>Basados en estos conceptos, surgen varias metodologías ágiles.\
+    Las más comunes son <a href='HTTP://DOMINIO-3/scrum.html'>Scrum</a> (orientada a producto)\
+    y Kanban (orientada a servicio)</p>");
+
+
+
+});
